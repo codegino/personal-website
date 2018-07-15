88a440a57311744283999bb195de4de54145b47d
gihooh <carloginocatapang@gmail.com>
2018-07-15 17:21:05 +0800

    Improve auto typing a little bit.

--- src/components/banner/HomeBanner.jsx
+++ src/components/banner/HomeBanner.jsx
@@ -3,37 +3,25 @@ import styles from './HomeBanner.scss';
 
 const AutoTyper = (props: { text: string }) => {
   const { text } = props;
-  return <input type="text" value={text} />;
+  return <input type="text" value={text} style={{width: '100%'}}/>;
 };
 
 // eslint-disable-next-line
 class HomeBanner extends React.Component {
+  static staticProps = {
+    typeSpeed: 100,
+  };
+
   state = {
     inputMessage: '',
-    message1: 'Hello World!',
-    message2: 'Ako si Carlo Gino Catapang',
     i: 0,
+    list: ['Hi! How are you?', 'This is Gihooh. Your poging developer.', "I'm so pogi",'Pangit ka huhuhu I love you Mariel.', 'Pangit ka sobra. I love you Mariel.'],
+    index: 0,
   };
 
   componentDidMount() {
-    const { message1 } = this.state;
-    this.addMessage(message1);
+    this.autoType();
   }
-
-  addMessage = message => {
-    const { i } = this.state;
-
-    if (i < message.length) {
-      this.setState(prevState => ({
-        ...prevState,
-        i: prevState.i + 1,
-        inputMessage: prevState.inputMessage + message[i],
-      }));
-      this.sleep(() => this.addMessage(message), 50);
-    } else {
-      this.sleep(this.deleteMessage, 1000);
-    }
-  };
 
   deleteMessage = () => {
     const { i } = this.state;
@@ -48,10 +36,34 @@ class HomeBanner extends React.Component {
     }
   };
 
-  sleep = (func, ms) => new Promise(resolve => {
-    setTimeout(func, ms);
-    resolve();
-  });
+  async addMessage(message) {
+    const { i } = this.state;
+
+    if (i < message.length) {
+      await this.setState(prevState => ({
+        ...prevState,
+        i: prevState.i + 1,
+        inputMessage: prevState.inputMessage + message[i],
+      }));
+      await setTimeout(() => this.addMessage(message), HomeBanner.staticProps.typeSpeed);
+    } else {
+      await setTimeout(() => this.deleteMessage(), 200);
+    }
+  }
+
+  async autoType(delay = 0) {
+    console.log(delay)
+    const { index, list } = this.state;
+
+    if (index < list.length) {
+      await this.setState(prevState => ({
+        ...prevState,
+        index: prevState.index + 1,
+      }));
+      await setTimeout(() => this.addMessage(list[index]), delay);
+      this.autoType(delay + list[index].length * HomeBanner.staticProps.typeSpeed * 2.5)
+    }
+  }
 
   render() {
     const { inputMessage } = this.state;
