4ebdd1bdf9e79d6022694261b72ab5fb0dd81621
gihooh <carloginocatapang@gmail.com>
2018-07-18 10:39:13 +0800

    Updated LoadingIcons

--- src/components/loading/LoadingIcon.jsx
+++ src/components/loading/LoadingIcon.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import MyLogo from 'assets/images/logo.svg';
+import Reload from 'assets/images/reload.svg';
 import styles from './LoadingIcon.scss';
 
 type LoadingIconProps = {
@@ -14,7 +15,8 @@ const LoadingIcon = (props: LoadingIconProps) => {
 
   return (
     <div className={`${styles.container} ${className}`} style={style}>
-      <MyLogo width={size} height={size} />
+      <Reload className={styles.borderLogo} width={size * 2} height={size * 2 } />
+      <MyLogo className={styles.mainLogo} width={size} height={size} />
     </div>
   );
 };
