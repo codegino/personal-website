0866274c1815eb572caf2bba8f5baed63895fb3e
gihooh <carloginocatapang@gmail.com>
2018-08-03 23:31:23 +0800

    Added firebase

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+const functions = require('firebase-functions');
+const cors = require('cors')({
+  origin: true,
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    response.send("Hello from Gihooh's Firebase!");
+  });
+});
